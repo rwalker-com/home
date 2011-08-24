9d6a772b92b44e4d1e7b3ad06ee27380fc927062
Rob Walker <rwalker@codeaurora.org>
2011-08-24 13:17:07 -0700

    updated from ruffian

---
+++ scripts/8660_wcn_randmac.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+me=$(basename $0)
+
+old=/tmp/${me}.${$}
+new=/tmp/${me}.${$}.rand
+
+mac="00A0$(printf %04x%04x ${RANDOM} ${RANDOM})"
+
+adb pull /etc/firmware/wlan/volans/WCN1314_qcom_cfg.ini ${old} || exit $?
+
+echo setting mac to ${mac}
+
+sed "s/NetworkAddress=.*/NetworkAddress=${mac}/g;s/gEnableImps=1/gEnableImps=0/g;s/gEnableBmps=1/gEnableBmps=0/g" < ${old} > ${new} || exit ${?}
+
+adb shell mount -o remount,rw /dev/block/mmcblk0p12 /system
+adb push ${new} /etc/firmware/wlan/volans/WCN1314_qcom_cfg.ini
+
+read -p "reboot? [Y/n]: " answer
+
+if [ -z "${answer%%[Yy]*}" ]
+then
+  adb reboot
+fi
+
+
+
